a33febbad0a5a5e69168f988409294f93cfe49fd
João <91199065+joaoHMeireles@users.noreply.github.com>
2023-02-09 20:38:13 -0300

    vhoafbod

---
+++ Paginas/Scanner/index.js
@@ -0,0 +1,33 @@
+const pistasVistas = [false, false, false, false, false]
+let pistaMostrar = ""
+let acabouJogo = false
+
+for (const marker of document.getElementsByClassName('game-marker')) {
+    marker.addEventListener('markerFound', (e) => {
+        alert(e);
+    });
+}
+
+
+function verPista(valorMarcador) {
+    if (checarPistasAnteriores(valorMarcador)) {
+        return "Você não pode ver essa pista sem antes ter encontrado as anteriores a ela"
+    }
+
+    pistaMostrar = assasino.dicasAssasino[valorMarcador - 1]
+
+    if (valorMarcador == 5) {
+        acabouJogo = true
+    }
+}
+
+
+function checarPistasAnteriores(valorMarcador) {
+    for (let i = valorMarcador; i > 0; i--) {
+        if (!pistasVistas[i - 2]) {
+            return false
+        }
+    }
+
+    return true
+}
--- index.js
+++ index.js
@@ -126,30 +126,3 @@ function iniciarNovoJogo() {
     console.log(morto.nome);
     localStorage.setItem("MORTO", morto.nome);
 };
-
-
-const pistasVistas = [false, false, false, false, false];
-let pistaMostrar = "";
-let acabouJogo = false;
-
-function verPista(valorMarcador) {
-    if (checarPistasAnteriores(valorMarcador)) {
-        return "Você não pode ver essa pista sem antes ter encontrado as anteriores a ela";
-    };
-
-    pistaMostrar = assasino.dicasAssasino[valorMarcador - 1];
-
-    if (valorMarcador == 5) {
-        acabouJogo = true;
-    };
-};
-
-function checarPistasAnteriores(valorMarcador) {
-    for (let i = valorMarcador; i > 0; i--) {
-        if (!pistasVistas[i - 2]) {
-            return false;
-        };
-    };
-
-    return true;
-};
