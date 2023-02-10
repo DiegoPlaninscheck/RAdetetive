061bba6762b40b789f7a1452854ddad02a775564
Camilly de Souza Pessotti <camilly_pessotti@estudante.sc.senai.br>
2023-02-10 19:10:50 -0300

    Update script.js

--- Paginas/Personagens/script.js
+++ Paginas/Personagens/script.js
@@ -2,9 +2,9 @@ const peronagem = document.getElementsByClassName("containerCharacter");
 const fundo = document.getElementsByClassName("fundo");
 const modal = document.getElementsByClassName("modal")
 
-peronagem.addEventListener("click", abrirModal());
+peronagem.addEventListener("click", abrirModal(fundo, modal));
 
-function abrirModal() {
+function abrirModal(fundo, modal) {
     fundo.style.visibility="visible";
     modal.style.visibility="visible";
 }
