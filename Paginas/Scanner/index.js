dd6914a06c3688b82dccc665af10a67b8679fac6
João <91199065+joaoHMeireles@users.noreply.github.com>
2023-02-09 21:42:12 -0300

    Fé

--- Paginas/Scanner/index.js
+++ Paginas/Scanner/index.js
@@ -10,10 +10,15 @@ if(assasino.alguemAchou){
 }
 
 const texto1 = document.getElementById("texto1")
+const texto2 = document.getElementById("texto2")
+const texto3 = document.getElementById("texto3")
+const texto4 = document.getElementById("texto4")
+const texto5 = document.getElementById("texto5")
 texto1.setAttribute("value", pistaTexto)
-
-console.log(texto1.value);
-
+texto2.setAttribute("value", assasino.dicasAssasino[1])
+texto3.setAttribute("value", assasino.dicasAssasino[2])
+texto4.setAttribute("value", assasino.dicasAssasino[3])
+texto5.setAttribute("value", assasino.dicasAssasino[4])
 
  
 for (const marker of document.getElementsByClassName('game-marker')) {
