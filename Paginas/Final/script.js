3bafa582d97644050717a03d525b3ae0fe043f9a
João <91199065+joaoHMeireles@users.noreply.github.com>
2023-02-10 21:12:54 -0300

    feat: colocando texto final

--- Paginas/Final/script.js
+++ Paginas/Final/script.js
@@ -1,14 +1,31 @@
 let venceu = null;
+const morto = localStorage.getItem("MORTO")
 
 function confirmarEscolha() {
     const select = document.getElementById("nomes")
+    const div = document.getElementById("textoFinal")
+    let textoFinal = "";
 
-    const assasino = localStorage.getItem("ASSASINO")
+    const assasino = JSON.parse(localStorage.getItem("ASSASINO"))
+    const mensagemAssasino = assasino.menssagemFinal
 
-    if (assasino == select.value) {
+    if (assasino.nome == select.value) {
         venceu = true
+        textoFinal += "Parabéns você acertou\n" + mensagemAssasino.replace("PESSOAMORTA", morto)
     } else {
         venceu = false
+        textoFinal += "Parabéns você é inutil\n"
     }
 
+    div.innerText = textoFinal;
 }
+
+function checarOpcoes() {
+    for (const opcao of document.getElementsByClassName("option")) {
+        if (opcao.value == morto) {
+            opcao.style.display = "none"
+        }
+    }
+}
+
+checarOpcoes()
