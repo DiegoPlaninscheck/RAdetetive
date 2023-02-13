74fc7f846fcbf30da6b3614d47c13996a13b3c70
João <91199065+joaoHMeireles@users.noreply.github.com>
2023-02-13 13:15:42 -0300

    eeeeeeeeeeeeeeeeeeeeeeeeeee

--- Paginas/Final/script.js
+++ Paginas/Final/script.js
@@ -4,6 +4,7 @@ const morto = localStorage.getItem("MORTO")
 function confirmarEscolha() {
     const select = document.getElementById("nomes")
     const div = document.getElementById("textoFinal")
+    const link = document.createElement("a")
     let textoFinal = "";
 
     const assasino = JSON.parse(localStorage.getItem("ASSASINO"))
@@ -11,13 +12,18 @@ function confirmarEscolha() {
 
     if (assasino.nome == select.value) {
         venceu = true
-        textoFinal += "Parabéns você acertou\n" + mensagemAssasino.replace("PESSOAMORTA", morto)
+        textoFinal += "Parabéns você acertou\n\n" + mensagemAssasino.replace("PESSOAMORTA", morto)
     } else {
         venceu = false
-        textoFinal += "Parabéns você é inutil\n"
+        textoFinal += "Parabéns você é inutil"
     }
 
+    link.href = "../../index.html"
+    link.innerText = "Voltar ao menu inicial"
+    link.id = "link"
+
     div.innerText = textoFinal;
+    div.appendChild(link)
 }
 
 function checarOpcoes() {
