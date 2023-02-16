16636b3e20833a7261aaa3c4aeb6280998138fef
Camilly de Souza Pessotti <camilly_pessotti@estudante.sc.senai.br>
2023-02-16 14:58:51 -0300

    commit

--- Paginas/Personagens/script.js
+++ Paginas/Personagens/script.js
@@ -1,11 +1,13 @@
 const personagens = document.getElementsByClassName("containerCharacter");
 const fundo = document.getElementById("fundo");
+const modal = document.getElementById("modal");
 
-for (const personagem of personagens) {
-    personagem.addEventListener("click", abrirModal(fundo, modal));
-};
+// for (const personagem of personagens) {
+// };
 
-function abrirModal(fundo, modal) {
+personagens.addEventListener("click", abrirModal());
+
+function abrirModal() {
     fundo.style.display = "block";
     modal.style.display = "block";
 };
